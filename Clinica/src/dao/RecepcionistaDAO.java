60af93c643ef00a5d3019c40f1ae13dc61b69888
Kainã Magdiel <114326061+fushisuno@users.noreply.github.com>
2024-11-24 16:38:14 -0300

    Add files via upload

--- Clinica/src/dao/RecepcionistaDAO.java
+++ Clinica/src/dao/RecepcionistaDAO.java
@@ -1,85 +1,73 @@
 package dao;
 
-import model.Funcionario;
 import model.Recepcionista;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecepcionistaDAO {
 
-    public void createRecepcionista(Recepcionista recepcionista) throws ExceptionDAO {
-        String sql = "INSERT INTO Recepcionista (idFuncionario) VALUES (?);";
-        
-        try (Connection conn = new ConexaoBD().getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-        	stmt.setInt(1, recepcionista.getIdFuncionario());
+	public void createRecepcionista(Recepcionista recepcionista) throws ExceptionDAO {
+		String sql = "INSERT INTO Recepcionista (idFuncionario) VALUES (?);";
+		try (Connection conn = new ConexaoBD().getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, recepcionista.getIdFuncionario());
 			stmt.execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ExceptionDAO("Erro ao criar Recepcionista: " + e.getMessage());
-        }
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new ExceptionDAO("Erro ao criar Recepcionista: " + e.getMessage());
+		}
+	}
 
-    public void updateRecepcionista(Recepcionista recepcionista) throws ExceptionDAO {
-        String sql = "UPDATE Recepcionista SET login = ?, senha = ?, cargo = ? WHERE idRecepcionista = ?;";
-        
-        try (Connection conn = new ConexaoBD().getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, recepcionista.getLogin());
-            stmt.setString(2, recepcionista.getSenha());
-            stmt.setString(3, recepcionista.getCargo());
-            stmt.setInt(4, recepcionista.getIdRecepcionista());
+	public void updateRecepcionista(Recepcionista recepcionista) throws ExceptionDAO {
+		String sql = "UPDATE Recepcionista SET idFuncionario = ?;";
+		try (Connection conn = new ConexaoBD().getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, recepcionista.getIdFuncionario());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new ExceptionDAO("Erro ao excluir Recepcionista: " + e.getMessage());
+		}
+	}
 
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ExceptionDAO("Erro ao atualizar Recepcionista: " + e.getMessage());
-        }
-    }
+	public void deleteRecepcionista(int idRecepcionista) throws ExceptionDAO {
+		String sql = "DELETE FROM Recepcionista WHERE idRecepcionista = ?;";
+		try (Connection conn = new ConexaoBD().getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setInt(1, idRecepcionista);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new ExceptionDAO("Erro ao excluir Recepcionista: " + e.getMessage());
+		}
+	}
 
-    public void deleteRecepcionista(int idRecepcionista) throws ExceptionDAO {
-        String sql = "DELETE FROM Recepcionista WHERE idRecepcionista = ?;";
-        
-        try (Connection conn = new ConexaoBD().getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setInt(1, idRecepcionista);
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ExceptionDAO("Erro ao excluir Recepcionista: " + e.getMessage());
-        }
-    }
+	public Recepcionista getRecepcionistaByCpf(String cpf) throws ExceptionDAO {
+		String sql = "SELECT * FROM Pessoa p \r\n" + "JOIN Funcionario f ON p.idPessoa = f.idPessoa\r\n"
+				+ "JOIN Recepcionista r on f.idFuncionario = r.idFuncionario WHERE p.cpf = ?";
+		try (Connection conn = new ConexaoBD().getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, cpf);
+			ResultSet rs = stmt.executeQuery();
 
-    public Recepcionista getRecepcionistaByCpf(String cpf) throws ExceptionDAO {
-        String sql = "SELECT * FROM Pessoa p \r\n"
-        		+ "JOIN Funcionario f ON p.idPessoa = f.idPessoa\r\n"
-        		+ "JOIN Recepcionista r on f.idFuncionario = r.idFuncionario WHERE p.cpf = ?";
-        try (Connection conn = new ConexaoBD().getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, cpf);
-            ResultSet rs = stmt.executeQuery();
-            
-            if (rs.next()) {
-                Recepcionista recepcionista = new Recepcionista();
-                recepcionista.setIdRecepcionista(rs.getInt("idRecepcionista"));
-                recepcionista.setIdPessoa(rs.getInt("idPessoa"));
-                recepcionista.setNome(rs.getString("nome"));
-                recepcionista.setTelefone(rs.getString("telefone"));
-                recepcionista.setCpf(rs.getString("cpf"));
-				
-                return recepcionista;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ExceptionDAO("Erro ao buscar Recepcionista: " + e.getMessage());
-        }
-        return null;
-    }
-    
-    public List<Recepcionista> getAllRecepcionistas() throws ExceptionDAO{
+			if (rs.next()) {
+				Recepcionista recepcionista = new Recepcionista();
+				recepcionista.setIdRecepcionista(rs.getInt("idRecepcionista"));
+				recepcionista.setIdPessoa(rs.getInt("idPessoa"));
+				recepcionista.setNome(rs.getString("nome"));
+				recepcionista.setTelefone(rs.getString("telefone"));
+				recepcionista.setCpf(rs.getString("cpf"));
+
+				return recepcionista;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new ExceptionDAO("Erro ao buscar Recepcionista: " + e.getMessage());
+		}
+		return null;
+	}
+
+	public List<Recepcionista> getAllRecepcionistas() throws ExceptionDAO{
         String sql = "SELECT * FROM Pessoa p "
                  + "JOIN Funcionario f ON p.idPessoa = f.idPessoa "
                  + "JOIN Recepcionista r ON f.idFuncionario = r.idFuncionario";
@@ -109,4 +97,5 @@ public class RecepcionistaDAO {
   
         return recepcionistas;
     }
+
 }
